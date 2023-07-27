8723cf5b80edbd7e94de703354c5740bb96ab44c
雷承宇 <ray@omniguider.com>
2023-07-27 14:15:20 +0800

    changeLib

--- Package.swift
+++ Package.swift
@@ -8,16 +8,16 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "hakkaExpo",
+            name: "HakkaExpo2023-iOS",
             targets: ["HakkaExpo2023-iOS"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "hakkaExpo"),
+            name: "HakkaExpo2023-iOS"),
         .testTarget(
             name: "Demo",
-            dependencies: ["hakkaExpo"]),
+            dependencies: ["HakkaExpo2023-iOS"]),
     ]
 )
